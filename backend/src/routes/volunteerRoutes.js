e76cef32add348978a9b4281b5fea2461c264411
AdeliaJuliani <adeliajuliani8@gmail.com>
2026-04-24 14:45:20 +0700

    menambahkan code

--- backend/src/routes/volunteerRoutes.js
+++ backend/src/routes/volunteerRoutes.js
@@ -1,19 +1,27 @@
-// =========================================
-// volunteerRoutes.js - Punya: User 4
-// Tanggung jawab: Volunteer Recruitment
-// =========================================
-// TODO (User 4):
-//   - POST   /volunteers              -> Daftar jadi relawan di suatu proyek
-//   - GET    /volunteers/:projectId   -> Liat daftar relawan di proyek tertentu
-//   - PUT    /volunteers/:id/approve  -> Approve relawan (khusus admin/owner proyek)
-//   - DELETE /volunteers/:id          -> Batalin pendaftaran relawan
+import { Router } from "express";
+import {
+  joinProject,
+  getVolunteers,
+  approveVolunteer,
+  rejectVolunteer,
+  resignVolunteer
+} from "../controllers/volunteerController.js";
 
-import { Router } from 'express';
 const router = Router();
 
-// Tambahin endpoint lo di sini ya!
-// Contoh:
-// router.post('/', joinProjectController);
-// router.get('/:projectId', getVolunteersController);
+// daftar volunteer
+router.post("/", joinProject);
+
+// ambil data volunteer
+router.get("/", getVolunteers);
+
+// approve
+router.put("/:id/approve", approveVolunteer);
+
+// reject
+router.put("/:id/reject", rejectVolunteer);
+
+// resign
+router.delete("/:id", resignVolunteer);
 
 export default router;
